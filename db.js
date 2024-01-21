14256fffb90951e59ed46a2b6ce82d4ba5cb5716
Dhruv <negidhruv1701@gmail.com>
2024-01-21 19:13:19 +0530

    updated

--- db.js
+++ db.js
@@ -5,12 +5,16 @@ const monoURI = process.env.MONGO_URI;
 mongoose.set('strictQuery', false);
 
 const connectToMongo = async ()=>{
-    await mongoose.connect(monoURI);
+    try {
+        await mongoose.connect(monoURI);
 
-    if (mongoose.connection.readyState === 1) {
-        console.log('Connected to MongoDB database');
-    } else {
-        console.log('Failed to connect to MongoDB database');
+        if (mongoose.connection.readyState === 1) {
+            console.log('Connected to MongoDB database');
+        } else {
+            console.log('Failed to connect to MongoDB database');
+        }
+    }catch (e) {
+        console.log(e)
     }
 }
 
