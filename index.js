628fb54ec8c6c8a39f24c52fc190c9ae7280acf9
Dhruv <negidhruv1701@gmail.com>
2024-02-13 12:29:51 +0530

    Added functionality to sub and unsub from topic in fcm

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const connectToMongo = require("./db");
 const express = require('express');
 const cors = require('cors')
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./chatify-17-firebase-adminsdk-o0uge-daec6a9290.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 connectToMongo()
 const app = express()
@@ -17,6 +25,7 @@ app.use('/api/authen', require('./routes/authen'));
 app.use('/api/contact', require('./routes/contact'));
 app.use('/api/chat', require('./routes/chat'))
 app.use('/api/ai', require('./routes/hercai'))
+app.use('/api/fcm', require('./routes/fcm'))
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
---
+++ routes/fcm.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const router = express.Router()
+const fetchUser = require('../middleware/fetchUser')
+const {getMessaging} = require('firebase-admin/messaging');
+
+router.post('/subscribe',fetchUser,async (req,res)=> {
+    try {
+        const {token} = req.body;
+        if (!token) {
+            return res.status(400).json({
+                type: 'error',
+                message: "Please provide a valid token/topic"
+            })
+        }
+        const topic = req.user.id;
+        try {
+            getMessaging().subscribeToTopic(token, topic)
+                .then((response) => {
+                    console.log('Successfully subscribed to topic:', response);
+                })
+                .catch((error) => {
+                    console.log('Error subscribing to topic:', error);
+                });
+        }catch (e){
+            return res.status(500).json({
+                type: 'error',
+                message: "Sorry! Some error occurred, try again later."
+            })
+        }
+        return res.status(200).json({
+            type: 'success',
+            message: 'Subscribed to FCM'
+        })
+    } catch (e) {
+        return res.status(500).json({
+            type: 'error',
+            message: "Sorry! Some error occurred, try again later."
+        })
+    }
+})
+
+router.post('/unsubscribe',fetchUser,async (req,res)=> {
+    try {
+        const {token} = req.body;
+        if (!token) {
+            return res.status(400).json({
+                type: 'error',
+                message: "Please provide a valid token/topic"
+            })
+        }
+        const topic = req.user.id;
+        try {
+            getMessaging().unsubscribeFromTopic(token, topic)
+                .then((response) => {
+                    console.log('Successfully unsubscribed to topic:', response);
+                })
+                .catch((error) => {
+                    console.log('Error unsubscribing to topic:', error);
+                });
+        }catch (e) {
+            return res.status(500).json({
+                type: 'error',
+                message: "Sorry! Some error occurred, try again later."
+            })
+        }
+        return res.status(200).json({
+            type: 'success',
+            message: 'Unsubscribed to FCM'
+        })
+    } catch (e) {
+        return res.status(500).json({
+            type: 'error',
+            message: "Sorry! Some error occurred, try again later."
+        })
+    }
+})
+module.exports = router;
+
