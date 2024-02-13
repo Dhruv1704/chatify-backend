380a2f3409e866249418642cb181f9f13dd26145
Dhruv <negidhruv1701@gmail.com>
2024-02-13 14:41:22 +0530

    fcm added

--- routes/chat.js
+++ routes/chat.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const fetchUser = require('../middleware/fetchUser')
 const Chat = require('../models/Chat')
 const {body, validationResult} = require("express-validator");
-const { Hercai } = require('hercai');
-
+const {getMessaging} = require('firebase-admin/messaging');
+const User = require("../models/User")
 
 router.get('/getMessage', fetchUser, async (req,res)=>{
     try{
@@ -24,14 +24,15 @@ router.get('/getMessage', fetchUser, async (req,res)=>{
 router.post('/addMessage', fetchUser, [
     body('receiver', 'Please enter a receiver').exists(),
     body('content', 'Please enter a message').exists(),
-    body('type', 'Please enter a type').exists()
+    body('type', 'Please enter a type').exists(),
+    body('receiverName', 'Please enter the receiver name').exists(),
 ], async (req,res)=>{
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({type: "error", message: errors.array()});
         }
-        const { receiver, content, type} = req.body;
+        const { receiver, content, type, receiverName} = req.body;
         const message = {
             sender: req.user.id,
             receiver,
@@ -39,6 +40,28 @@ router.post('/addMessage', fetchUser, [
             type
         }
         await Chat.create(message);
+        const topic = receiver;
+
+        const messageFCM = {
+            data: {
+                title: receiverName,
+                body: content,
+                image: "https://firebasestorage.googleapis.com/v0/b/chatify-17.appspot.com/o/app-image%2Ficon_x512-modified.png?alt=media&token=3192bd5a-4a8b-4598-826f-cd8339c3ca0c",
+                badge: "https://firebasestorage.googleapis.com/v0/b/chatify-17.appspot.com/o/app-image%2Ficon_x512-modified.png?alt=media&token=3192bd5a-4a8b-4598-826f-cd8339c3ca0c",
+            },
+            topic: topic
+        };
+
+        // Send a message to devices subscribed to the provided topic.
+        getMessaging().send(messageFCM)
+            .then((response) => {
+                // Response is a message ID string.
+                console.log('Successfully sent message:', response);
+            })
+            .catch((error) => {
+                console.log('Error sending message:', error);
+            });
+
         return res.status(200).json({
             type: "success",
             message: "Chat Added Successfully"
